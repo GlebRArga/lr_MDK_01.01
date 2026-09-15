c23d00a2b4da58308f296e8ba5d0dc748ebf4836
Глеб <gleb.rumantsev.06@mail.ru>
2026-09-15 22:55:08 +0300

    создал исключение для максимального пульса

--- LR1/Variant7/Variant7/Program.cs
+++ LR1/Variant7/Variant7/Program.cs
@@ -26,7 +26,6 @@ namespace Variant7
             Console.Write("Введите цель тренеровки (1-жирожигание,2-кардиотренеровка,3-повышение выносливости): ");
             string target = Console.ReadLine();
             maxPuls = 220 - age;
-            Console.WriteLine("Максимальный пульс: " + maxPuls);
             TargetHearRate(maxPuls, target);
         }
 
@@ -37,18 +36,21 @@ namespace Variant7
             double recommendPulsMax;
             if (target == "1")
             {
+                Console.WriteLine("Максимальный пульс: " + maxPuls);
                 recommendPulsMin = Math.Ceiling(maxPuls * 0.6);
                 recommendPulsMax = Math.Floor(maxPuls * 0.7);
                 Console.WriteLine("Рекомендуемый пульс: " + recommendPulsMin + "-" + recommendPulsMax + "уд/мин");
             }
             else if (target == "2")
             {
+                Console.WriteLine("Максимальный пульс: " + maxPuls);
                 recommendPulsMin = Math.Ceiling(maxPuls * 0.7);
                 recommendPulsMax = Math.Floor(maxPuls * 0.8);
                 Console.WriteLine("Рекомендуемый пульс: " + recommendPulsMin + "-" + recommendPulsMax + "уд/мин");
             }
             else if (target == "3")
             {
+                Console.WriteLine("Максимальный пульс: " + maxPuls);
                 recommendPulsMin = Math.Ceiling(maxPuls * 0.8);
                 recommendPulsMax = Math.Floor(maxPuls * 0.9);
                 Console.WriteLine("Рекомендуемый пульс: " + recommendPulsMin + "-" + recommendPulsMax + "уд/мин");
